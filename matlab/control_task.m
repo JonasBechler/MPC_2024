cd5393191219f01f018b51cd1a0b7049e68048ff
Jonas Bechler <jonasbechler@gmail.com>
2024-06-07 21:57:50 +0200

    much better

--- matlab/control_task.m
+++ matlab/control_task.m
@@ -3,13 +3,28 @@ clear;
 clc;
 close all;
 
+plant = 1;
 
-d = 0; % plant delay
-B = [0.0281, 0.1278, 0.0513, 0.0013];
-B = [0.4, 0.6];
+if plant == 1
+    % Main Plant for Task
+    d = 1; % plant delay
+    B = [0.0281, 0.1278, 0.0513, 0.0013];
+    A = [1, -1.2254, 0.5711, -0.3507, 0.005];
+    C = [1 -0.9];
+    D = [1, -1];
 
-A = [1, -1.2254, 0.5711, -0.3507, 0.005];
-A = [1, -0.8];
+elseif plant == 2
+    % Simple Plant Example from Camacho S56
+    d = 0; % plant delay
+    B = [0.4, 0.6];
+    A = [1, -0.8];
+    C = [1];
+    D = [1, -1];
+end
+
+
+o_a = length(A) - 1;
+o_b = length(B) - 1;
 
 
 figure();
@@ -27,89 +42,110 @@ title("Step response");
 C = [1];
 D = [1, -1];
 
-h_p = zeros(1, 4);
-h_i = zeros(1, 4);
-h_c = zeros(1, 4);
-l = zeros(1, 4);
+o_c = length(C) - 1;
+o_d = length(D) - 1;
 
-% property 1 (Minimal Variance Control)
-% h_p = h_i = plant_delay + 1
-% h_c = 1
-% l = 0
-h_p(1) = d + 1;
-h_i(1) = d + 1;
-h_c(1) = 1;
-l(1) = 0;
 
-% property 2 (One step Ahead Predictive Control)
-% h_p = h_i = plant_delay + 1
-% h_c = 1
-% l != 0
-h_p(2) = d + 1;
-h_i(2) = d + 1;
-h_c(2) = 1;
-l(2) = 0.9; % not 0
 
-% property 3 (Pole placement)
-% h_c = order(A) + order(D)
-% h_i = order(B) + plant_delay + 1
-% h_p > h_i + h_c
-% l = 0
-h_c(3) = length(A) + length(D) - 2;
-h_i(3) = length(B) + d;
-h_p(3) = h_i(3) + h_c(3);
-l(3) = 0;
 
-% property 4 (Internal Model Control)
-% h_p -> inf (100+)
-% h_c = 1
-% h_i = plant_delay + 1
-% l = 0
-% D = 1 - q^-1
-h_p(4) = 10;
-h_i(4) = d + 1;
-h_c(4) = h_p(4) - h_i(4);
-l(4) = 0;
 
-N = 2;
 
-% N1 = start prediction
-% N2 = end prediction
 
- %h_i(1) = d+1;
- %h_p(1) = d+N+1;
- %h_c(1) = N+1;
- %l(1) = 0.1;
-%
- %h_i(2) = d+2;
- %h_p(2) = d+N+1;
- %h_c(2) = N;
- %l(2) = 0.5;
-%
- %h_i(3) = d+3;
- %h_p(3) = d+N+1;
- %h_c(3) = N-1;
- %l(3) = 0.9;
+test_delay = false;
+test_hc = true;
 
 for i = 1:4
+    
+    if test_delay
+        d = i-1;
+        N = 5;
+        hi = d + 1;
+        hp = d + N + 1;
+        hc = N + 1;
+        lambda = 0.8;
 
-    E_ges = zeros(h_c(i), h_c(i));
-    F_size = length(A) + length(D)-2;
-    F_ges = zeros(h_c(i), F_size);
-    G_ges = zeros(h_c(i), h_c(i));
-    H_size = length(C);
-    H_ges = zeros(h_c(i), H_size);
+    elseif test_hc
+        d = 0;
+        N = i*i*i
+        hi = d + 1;
+        hp = d + N + 1;
+        hc = N+1;
+        lambda = 0.8;
 
-    for j = h_i(i):h_p(i)
-        [E, F] = PolyDiv(C, conv(A, D), j);
-        E_ges(j-h_i(i)+1, 1:length(E)) = E;
-        F_ges(j-h_i(i)+1, 1:length(F)) = F;
 
-        [G, H] = PolyDiv(conv(B, E), C, j);
-        G_ges(j-h_i(i)+1, 1:length(G)) = G;
-        H_ges(j-h_i(i)+1, 1:length(H)) = H;
+    else
+        
+        
+        if i == 1
+            % property 1 (Minimal Variance Control)
+            % h_p = h_i = plant_delay + 1
+            % h_c = 1
+            % l = 0
+            hp = d + 1;
+            hi = d + 1;
+            hc = 1;
+            lambda = 0.00;
+            
+        elseif i == 2
+            % property 2 (One step Ahead Predictive Control)
+            % h_p = h_i = plant_delay + 1
+            % h_c = 1
+            % l != 0
+            hp = d + 1;
+            hi = d + 1;
+            hc = 1;
+            lambda = 0.8;
+            
+        elseif i == 3
+            % property 3 (Pole placement)
+            % h_c = order(A) + order(D)
+            % h_i = order(B) + plant_delay + 1
+            % h_p > h_i + h_c
+            % l = 0
+            hc = o_a + o_d;
+            hi = o_b + d + 1;
+            hp = hi + hc + 1;
+            lambda = 0.00;
+            
+        elseif i == 4
+            % property 4 (Internal Model Control)
+            % h_p -> inf (100+)
+            % h_c = 1
+            % h_i = plant_delay + 1
+            % l = 0
+            % D = 1 - q^-1
+            hp = 10;
+            hc = d + 1;
+            hi = d + 1;
+            lambda = 0.00;
+        end
+        
     end
+    
+    disp("d: " + d + " hp: " + hp + " hi: " + hi + " hc: " + hc + " lambda: " + lambda);
+    
+    
+    
+    E_ges = zeros(hp, hp);
+    F_size = o_a + o_d;
+    F_ges = zeros(hp, F_size);
+    G_ges = zeros(hp, hp);
+    G_ges_flipped = zeros(hp, hp);
+    H_size = length(C);
+    H_ges = zeros(hp, H_size);
+    
+    for j = 1:hp
+        [E, F] = PolyDiv(C, conv(A, D), j);
+        E_ges(j, 1:length(E)) = E;
+        F_ges(j, 1:length(F)) = F;
+        
+        [G, H] = PolyDiv(conv(B, E), C, j);
+        G_ges(j, 1:length(G)) = G;
+        H_ges(j, 1:length(H)) = H;
 
+        G_ges_flipped(j, 1:length(G)) = fliplr(G);
+    end
+    
     disp("E_ges:")
     disp(E_ges)
     disp("F_ges:")
@@ -119,22 +155,39 @@ for i = 1:4
     disp("H_ges:")
     disp(H_ges)
 
-    % psi:
+    % G_ges:
+
+    % [g_0_0,       0,      0,      0]
+    % [g_1_0,   g_1_1,      0,      0]
+    % [g_2_0,   g_2_1,  g_2_2,      0]
+    % [g_3_0,   g_3_1,  g_3_2,  g_3_3]
+
+    % G_ges_flipped:
+    
     % [g_0_0,       0,      0,      0]
     % [g_1_1,   g_1_0,      0,      0]
     % [g_2_2,   g_2_1,  g_2_0,      0]
     % [g_3_3,   g_3_2,  g_3_1,  g_3_0]
     
-    psi = zeros(h_c(i), h_c(i));
+    % psi:
+    % [g_1_1,   g_1_0,      0]
+    % [g_2_2,   g_2_1,  g_2_0]
+    % [g_3_3,   g_3_2,  g_3_1]
+
     
-    for j = h_i(i):h_p(i)
-        psi(j-h_i(i)+1, min(j, h_c(i)):-1:1) = G_ges(j-h_i(i)+1, max(1, j - h_c(i) + 1):j);
-    end
+    psi = G_ges_flipped(hi:hp, 1:hc);
+    
+    % ?????????????????
+    %for j = hi:hp
+    %    buf = G_ges(j, :);
+    %    buf = buf.
+    %    % psi(j-hp+1, min(j, hp):-1:1) = G_ges(j, max(1, j - hp + 1):j);
+    %end
     
     disp("psi:")
     disp(psi)
     
-    gamma = (psi' * psi + l(i) * eye(h_c(i))) \ psi';
+    gamma = (psi' * psi + lambda * eye(hc)) \ psi';
     gamma = gamma(1, :);
     
     disp("gamma:")
@@ -146,54 +199,97 @@ for i = 1:4
     
     % R = sum(j = [0, ..., h_c])(gamma_j * F_j)
     R = zeros(1, length(F_ges(1, :)));
-    for j = 1:h_c(i)
-        R = R + gamma(j) * F_ges(j, :);
+    for j = hp:hp
+        R = R + gamma(j - hp + 1) * F_ges(j, :);
     end
+    disp("R:")
+    disp(R)
     
     % S = D * { C + sum(j = [0, ..., h_c])(gamma_j * H_j-d) * q^-1}
-    %   = D * C + D * sum(j = [0, ..., h_c])(gamma_j * H_j-d) * q^-1
     
-    S1 = conv(D, C);
+    S1 = C;
     S2 = zeros(1, length(H_ges(1,:)));
-    for j = 1:h_c(i)
-        S2 = S2 + gamma(j) * H_ges(j, :);
+    for j = hp:hp
+        S2 = S2 + gamma(j - hp + 1) * H_ges(j - d, :);
     end
-    S2 = conv(D, S2);
     S2 = [0, S2];
+    
     if length(S1) < length(S2)
         S1 = [S1, zeros(1, length(S2) - length(S1))];
     else
         S2 = [S2, zeros(1, length(S1) - length(S2))];
     end
     S = S1 + S2;
+    S = conv(D, S);
     
-    % T = C * sum(j = [0, ..., h_c])(gamma_j * q^-j)
-    T = C * gamma;
-    
-    disp("R:")
-    disp(R)
     disp("S:")
     disp(S)
+    
+    % T = C * sum(j = [0, ..., h_c])(gamma_j * q^-j)
+    T = conv(C, gamma);
     disp("T:")
     disp(T)
+    
+    
     
     % p_c = A * S + B * R * q^(-d-1)
     p_c1 = conv(A, S);
     p_c2 = [zeros(1, d+1), conv(B, R)];
+    disp("p_c1:")
+    disp(p_c1)
+    disp("p_c2:")
+    disp(p_c2)
     if length(p_c1) < length(p_c2)
         p_c1 = [p_c1, zeros(1, length(p_c2) - length(p_c1))];
     else
         p_c2 = [p_c2, zeros(1, length(p_c1) - length(p_c2))];
     end
     p_c = p_c1 + p_c2;
+    %p_c = p_c(1:find(p_c, 1, 'last'));
+
+    disp("p_c:")
+    disp(p_c)
     
-    
-    y_ys = tf(conv(B, T), [p_c, zeros(1, d)], -1);
+    % y_ys = B * T * q^(-d-1) / p_c
+    y_ys_n = [zeros(1, d+1), conv(B, T)];
+    y_ys_d = p_c;
+    if length(y_ys_n) < length(y_ys_d)
+        y_ys_n = [y_ys_n, zeros(1, length(y_ys_d) - length(y_ys_n))];
+    else
+        y_ys_d = [y_ys_d, zeros(1, length(y_ys_n) - length(y_ys_d))];
+    end
+    disp("y_ys:")
+    disp(y_ys_n)
+    disp(y_ys_d)
+    y_ys = tf(y_ys_n, y_ys_d, -1);
+
+    % u_ys = A * T / p_c
+    u_ys_n = conv(A, T);
+    u_ys_d = p_c;
+    if length(u_ys_n) < length(u_ys_d)
+        u_ys_n = [u_ys_n, zeros(1, length(u_ys_d) - length(u_ys_n))];
+    else
+        u_ys_d = [u_ys_d, zeros(1, length(u_ys_n) - length(u_ys_d))];
+    end
+    disp("u_ys:")
+    disp(u_ys_n)
+    disp(u_ys_d)
+    u_ys = tf(u_ys_n, u_ys_d, -1);
+
+
+
+
+    AT = conv(A, T);
+    AS = conv(A, S);
+    BS = conv(B, S);
+    AR = conv(A, R);
+    BR = conv(B, R);
+
     y_vy = tf(conv(A, S), p_c, -1);                               % Sensitivity function
     y_vu = tf(conv(B, S), [p_c, zeros(1, d+1)], -1);     % Sensitivity function x System
     y_n  = tf(conv(B, R), [p_c, zeros(1, d+1)], -1);     % Complementary sensitivity function
     
-    u_ys =   tf(conv(A, T), p_c, -1);
+    %u_ys =   tf(conv(A, T), p_c, -1);
     u_vy = - tf(conv(A, R), p_c, -1);                         % Sensitivity function x Controller
     u_vu =   tf(conv(B, R), [p_c, zeros(1, d+1)], -1);                           % Sensitivity function
     u_n  = - tf(conv(A, R), p_c, -1);                         % Sensitivity function x Controller
@@ -202,204 +298,17 @@ for i = 1:4
     pzmap(y_ys);
     title("Poles and Zeros Y/Y*");
     
+    
     subplot(4,5, i+1 +5);
-    step(y_ys, 50);
+    step(y_ys);
     hold on;
-    step(u_ys, 50);
+    step(u_ys);
     grid on;
     title("Step response Y/Y* and U/Y*");
     xlabel("Time");
     ylabel("Amplitude");
-    ylim([-0.2, 1.2]);
     
     
     
     
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
